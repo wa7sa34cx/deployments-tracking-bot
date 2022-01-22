1bfc209376fd19e79ff18089e1ae6cc08ee694dd
Alex Black <wa7@sa34.cx>
2022-01-22 22:07:36 +0300

    Still writing...

--- src/digitalocean/apps.rs
+++ src/digitalocean/apps.rs
@@ -1,4 +1,8 @@
-//! List all apps on account.
+//! Module for retrieving list of all applications in the account with last deployment.
+
+use chrono::NaiveDateTime;
+use reqwest::header;
+use serde_derive::*;
 
 use crate::digitalocean::DigitalOcean;
 
@@ -6,13 +10,19 @@ use crate::digitalocean::DigitalOcean;
 pub struct App {
     pub id: String,
     pub name: String,
-    pub created_at: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
 
+// https://docs.digitalocean.com/reference/api/api-reference/#operation/list_apps
+#[derive(Deserialize)]
+pub struct JsonResponse {
+    apps: Vec<JsonApp>
 }
 
 impl DigitalOcean {
     /// Gets list of apps
-    pub fn apps(&self) -> Vec<App> {
+    pub fn get_apps(&self) -> Vec<App> {
         vec![App {
             internal: "1".to_string(),
         },
@@ -20,4 +30,24 @@ impl DigitalOcean {
             internal: "2".to_string(),
         }]
     }
+}
+
+
+// Gets list all Apps
+async fn get_user_data(
+    access_token: impl Into<String>,
+    client: &Client,
+) -> anyhow::Result<UserData> {
+    let user_data = client
+        .get("https://discord.com/api/users/@me")
+        .header(
+            header::AUTHORIZATION,
+            &format!("Bearer {}", access_token.into()),
+        )
+        .send()
+        .await?
+        .json::<UserData>()
+        .await?;
+
+    Ok(user_data)
 }
