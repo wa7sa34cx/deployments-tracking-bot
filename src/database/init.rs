b7b52d28bdb89e6ec631c3579b5e93b6c12b9fb9
Alex Black <wa7@sa34.cx>
2022-01-24 18:49:50 +0300

    Add some log messages

--- src/database/init.rs
+++ src/database/init.rs
@@ -1,18 +1,33 @@
 //! Database initialization
 
 use std::path::PathBuf;
+use std::io;
 use tokio::fs;
 
 use crate::database::Database;
 
 impl Database {
     // Creates new Db instance
-    pub async fn init() -> anyhow::Result<Self> {
+    pub async fn init() -> Result<Self, io::Error> {
         let path = PathBuf::from(dotenv::var("DB_PATH").unwrap());
 
-        // Remove the old DB and create an empty new one
-        fs::remove_dir_all(&path).await?;
+        // Remove the old database
+        match fs::remove_dir_all(&path).await {
+            Ok(_) => {
+                log::warn!("The old database has been deleted. Creating...");
+            },
+            Err(e) => {
+                if e.kind() == io::ErrorKind::NotFound {
+                    log::warn!("The database does not exist. Creating...");
+                } else {
+                    return Err(e);
+                }
+            },
+        };
+
+        // And create an empty new one
         fs::create_dir(&path).await?;
+        log::info!("The database has been created");
 
         Ok(Self {
             path,
