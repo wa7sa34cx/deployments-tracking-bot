31b6516d3855885b3ceb4e1d0f8bab74fa71904b
Alex Black <wa7@sa34.cx>
2022-01-24 21:13:26 +0300

    Add modules

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -3,8 +3,13 @@
 use std::path::PathBuf;
 
 pub mod init;
-pub mod create_table;
+pub mod table;
+pub mod table_create;
+pub mod table_exists;
+pub mod table_read;
+pub mod table_write;
 
+/// Main database struct
 #[derive(Debug)]
 pub struct Database {
     path: PathBuf,
