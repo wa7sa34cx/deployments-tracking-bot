0eac7c43bc7da4644c355f07ee6706aa6c4b5106
Alex Black <wa7@sa34.cx>
2022-01-24 21:12:43 +0300

    Add table module

---
+++ src/database/table.rs
@@ -0,0 +1,25 @@
+//! Creating table instance
+
+use std::path::PathBuf;
+
+use crate::database::Database;
+
+#[derive(Debug)]
+pub struct Table {
+    pub name: String, 
+    pub file: PathBuf,
+}
+
+impl Database {
+    /// Creates a new table in the database
+    pub fn table(&self, name: &str) -> Table {
+
+        let mut file = PathBuf::from(self.path.as_path());
+        file.push(&name);
+
+        Table {
+            name: name.to_string(),
+            file,
+        }
+    }
+}
