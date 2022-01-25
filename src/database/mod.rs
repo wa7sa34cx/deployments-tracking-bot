cbb6e64665d5813ea7e3a31f9d789c99fc0c7442
Alex Black <wa7@sa34.cx>
2022-01-25 18:06:13 +0300

    New init and from_env

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -7,8 +7,15 @@ use std::sync::Arc;
 pub mod init;
 pub mod table;
 
-/// Main database struct
-pub struct Database(Arc<SharedDatabase>);
+/// Main Database struct
+#[derive(Debug)]
+pub struct Database(Arc<DatabaseConfig>);
+
+/// Database configuration
+#[derive(Debug)]
+pub struct DatabaseConfig {
+    path: PathBuf,
+}
 
 impl Clone for Database {
     fn clone(&self) -> Self {
@@ -17,15 +24,24 @@ impl Clone for Database {
 }
 
 impl Deref for Database {
-    type Target = SharedDatabase;
+    type Target = DatabaseConfig;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-/// Internal struct for safe multithreading
-#[derive(Debug)]
-pub struct SharedDatabase {
-    path: PathBuf,
+impl Database {
+    /// Creates DatabaseConfig
+    ///
+    /// # Panics
+    ///
+    /// Panics if the DB_PATH variable are not specified in environment
+    pub fn from_env() -> DatabaseConfig {
+        let path = PathBuf::from(dotenv::var("DB_PATH").unwrap());
+
+        DatabaseConfig { path }
+    }
 }
+
+
