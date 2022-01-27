38e706a42f565cd0283da2dfa47849c9d5bf6ae8
Alex Black <wa7@sa34.cx>
2022-01-27 13:48:23 +0300

    Telegram module

--- src/telegram/init.rs
+++ src/telegram/init.rs
@@ -1,1 +1,50 @@
 //! Telegram initialization.
+
+use serde_derive::Deserialize;
+use std::sync::Arc;
+
+use crate::telegram::{Telegram, TelegramConfig};
+
+// https://core.telegram.org/bots/api#making-requests
+#[derive(Debug, Deserialize)]
+pub struct JsonResponse {
+    pub ok: bool,
+    pub result: Option<User>,
+    pub description: Option<String>,
+}
+
+// https://core.telegram.org/bots/api#user
+#[derive(Debug, Deserialize)]
+pub struct User {
+    pub id: i64,
+    pub is_bot: bool,
+}
+
+impl TelegramConfig {
+    // Initializes working with Telegram API, checks bot status
+    pub async fn init(self) -> anyhow::Result<Telegram> {
+        let url = format!("{}getMe", &self.url);
+
+        let json = self
+            .client
+            .get(&url)
+            .send()
+            .await?
+            .json::<JsonResponse>()
+            .await?;
+
+        if !json.ok {
+            return anyhow::anyhow!("token authentication failed");
+        }
+        
+        match json.result {
+            Some(r) => if !r.is_bot { return anyhow::anyhow!("this is not a bot") },
+            None => return anyhow::anyhow!("what the heck?"),
+        }
+
+        Ok(Telegram(Arc::new(TelegramConfig {
+            url: self.url,
+            client: self.client,
+        })))
+    }
+}
--- src/telegram/mod.rs
+++ src/telegram/mod.rs
@@ -1,7 +1,55 @@
-//! Telegram Module.
+//! Telegram module.
+//! 
+//! A new era of messaging 💬
+
+use reqwest::Client;
+use std::ops::Deref;
+use std::sync::Arc;
 
 pub mod init;
 
+static TG_BOT_API_URL: &str = "https://api.telegram.org/bot";
+
 /// Main Telegram struct
 #[derive(Debug)]
-pub struct Telegram {}
+pub struct Telegram(Arc<TelegramConfig>);
+
+/// Telegram configuration
+#[derive(Debug)]
+pub struct TelegramConfig {
+    url: String,
+    client: Client,
+}
+
+impl Clone for Telegram {
+    fn clone(&self) -> Self {
+        Self(Arc::clone(&self.0))
+    }
+}
+
+impl Deref for Telegram {
+    type Target = TelegramConfig;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Telegram {
+    /// Creates TelegramConfig from environment
+    ///
+    /// # Panics
+    ///
+    /// Panics if the TG_TOKEN variable are not specified in environment
+    pub fn from_env() -> TelegramConfig {
+        let token = dotenv::var("TG_TOKEN").unwrap();
+
+        // https://core.telegram.org/bots/api#making-requests
+        let url = format!("{}{}/", TG_BOT_API_URL, &token);
+
+        // Create keep-alive HTTP connection pool
+        let client = Client::new();
+
+        TelegramConfig { url, client }
+    }
+}
